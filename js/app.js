5a85c394c4e61d2e65a0c2625ddb82ef3d29ab14
Richard Huang <flyerhzm@gmail.com>
2022-02-01 16:26:49 +0800

    style current nav link

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  var currentLinks = document.querySelectorAll('a[href="' + window.location.pathname + '"]');
+  currentLinks.forEach(function(link) { link.classList.add('current'); });
+
   var language = window.location.href.split('/')[3];
   var element = document.querySelector('.languages .' + language);
   if (element) {
