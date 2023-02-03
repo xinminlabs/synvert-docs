345f48b7069af6787b1a3628e96bf8e48db88753
Richard Huang <flyerhzm@gmail.com>
2023-02-03 22:51:16 +0800

    remove unused languages

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var currentLinks = document.querySelectorAll('a[href="' + window.location.pathname + '"]');
   currentLinks.forEach(function(link) { link.classList.add('current'); });
-
-  var language = window.location.href.split('/')[3];
-  var languageSelect = document.querySelector('#languages');
-  languageSelect.addEventListener('change', function() {
-    window.location.href = '/' + languageSelect.value + '/home/';
-  });
-  var element = languageSelect.querySelector('option[value="' + language + '"]');
-  element.selected = 'selected';
 });
